efb7930ea173df70009ec9ce9677ca0725cf82d1
atrox3d <atrox3d@gmail.com>
2024-07-05 20:16:52 +0200

    test against fixed literal, not logic

--- tests/test_circle.py
+++ tests/test_circle.py
@@ -14,13 +14,18 @@ class TestCircle:
 
     def setup_method(self, method):
         print(f'setting up METHOD{method}')
+        self.circle = shapes.Circle(10)
 
-    def test_one(self):
-        print(f'testing test_one')
-        assert True
+    def test_area(self):
+        assert self.circle.area() == 314.1592653589793
+
+    def test_perimeter(self):
+        print(f'testing test_two')
+        assert self.circle.perimeter() == 62.83185307179586
     
     def teardown_method(self, method):
         print(f'tearing down METHOD {method}')
+        del self.circle
     
     def teardown_class(cls):
         print(f'tearing down CLASS {cls}')
