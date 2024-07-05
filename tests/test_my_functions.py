296d0e49371c79817e16812d2e6db94e41b07eee
atrox3d <atrox3d@gmail.com>
2024-07-05 19:07:46 +0200

    add setup/teardown to functions

--- tests/test_my_functions.py
+++ tests/test_my_functions.py
@@ -1,6 +1,18 @@
 import pytest
 import source.my_functions as my_functions
 
+def setup_module(module):
+    print(f'setting up MODULE {module}')
+
+def teardown_module(module):
+    print(f'tearing down MODULE {module}')
+
+def setup_function(function):
+    print(f'setting up FUNCTION {function}')
+
+def teardown_function(function):
+    print(f'tearing down function {function}')
+
 def test_add():
     result = my_functions.add(1, 4)
     assert result == 5
